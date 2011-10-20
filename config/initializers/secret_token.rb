7f9fcdff67451ac11284c502e8b6b61ca25af6c4
Deke Brown <dknbrown@gmail.com>
2011-10-20 12:13:32 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Photobot2000::Application.config.secret_token = '98d1305d78d1d0067b7489f59348c622dacc17c4092f40f91788b039c1a05d3f7aeb4a537ccba80c2fb605219cc049313ce6b70ed52cf4b920bb56fdebe51b49'
